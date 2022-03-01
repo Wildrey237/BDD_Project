ecf9e5f9e4158b18e0d84a6611a54cb7f6a7669b
Wildrey237 <wilfriedbemelingue@gmail.com>
2022-03-01 10:23:18 +0100

    le connect data base est effectif

--- connectDB.py
+++ connectDB.py
@@ -1,9 +1,29 @@
 import mysql.connector
 
-if __name__ == '__main__':
-    db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="happyFly"
-    )
+class Singleton:
+    def __init__(self, cls):
+        self._cls = cls
+    def Instance(self):
+        try:
+            return self._instance
+        except AttributeError:
+            self._instance = self._cls()
+            return self._instance
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `Instance()`.')
+    def __instancecheck__(self, instance):
+        return isinstance(instance,self._cls)
+@Singleton
+class DBSingleton:
+    def __init__(self):
+        self.conn = mysql.connector.connect(user='root', password='root', host='localhost', database='happyFly')
+        pass
+    def query(self, sql, params: tuple = ()):
+        cursor = self.conn.cursor()
+        cursor.execute(sql, params)
+        result = cursor.fetchall()
+        self.conn.commit()
+        cursor.close
+        return result
+    def __str__(self):
+        return 'Data connection object'
